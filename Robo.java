462917809918a506e048bcb4bf5b1098482ed7fc
Júlia Cotrim Chinaglia <136257249+JuliaCotrim18@users.noreply.github.com>
2025-03-25 14:18:47 -0300

    Robo.java

--- Robo.java
+++ Robo.java
@@ -1,28 +1,37 @@
 class Robo
 {
     // atributos da classe
-    String nome;
-    int posicaoX;
-    int posicaoY;
+    private String nome;
+    private String direcao;
+    private int posicaoX;
+    private int posicaoY;
 
     // método construtor
-    Robo(String nome, int posicaoX, int posicaoY)
+    Robo(String nome, String direcao, int posicaoX, int posicaoY)
     {
         this.nome = nome;
+        this.direcao = direcao; //(Norte, Leste, Sul, Oeste).
         this.posicaoX = posicaoX;
         this.posicaoY = posicaoY;
     }
 
     // atualiza a posição do robô
-    void mover(int deltaX, int deltaY)
+    public void mover(int deltaX, int deltaY)
     {
         this.posicaoX += deltaX;
         this.posicaoY += deltaY;
     }
 
+    // identifica obstáculos ao redor do robô
+    public void identificarObstaculo()
+    {
+        // não implementado
+    }
+
+
     // retorna uma string que pode ser utilizada em outras interfaces
     // é utilizada na classe Main para exibir a posição
-    String exibirPosicao()
+    public String exibirPosicao()
     {
         // retorna a string "(x, y)" onde x é this.posicaoX, etc.
         return "(" + this.posicaoX + ", " + this.posicaoY + ")";
