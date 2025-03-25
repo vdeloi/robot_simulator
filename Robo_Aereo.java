a44e8daa863d13920f2327597dfaa1856d640734
Júlia Cotrim Chinaglia <136257249+JuliaCotrim18@users.noreply.github.com>
2025-03-25 14:20:35 -0300

    Robo_Aereo.java

---
+++ Robo_Aereo.java
@@ -0,0 +1,23 @@
+public class RoboAereo extends Robo{
+
+    private int altitude;
+    private int altitude_maxima;
+
+    public RoboAereo(String nome, String direcao, int posicaoX, int posicaoY, int altitude_maxima)
+    {
+        super(nome, direcao, posicaoX, posicaoY);
+        this.altitude = 0;
+        this.altitude_maxima = altitude_maxima;
+    }
+
+    public void subir (){
+
+    }
+
+    public void descer (){
+
+        
+    }
+
+
+}
