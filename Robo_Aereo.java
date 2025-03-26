b6f07e914131c7c9f1ac00263ab7d4fef5e9d99f
Júlia Cotrim Chinaglia <136257249+JuliaCotrim18@users.noreply.github.com>
2025-03-26 08:00:20 -0300

    Update Robo_Aereo.java
    função subir e descer adicionada :)

--- Robo_Aereo.java
+++ Robo_Aereo.java
@@ -10,14 +10,26 @@ public class RoboAereo extends Robo{
         this.altitude_maxima = altitude_maxima;
     }
 
-    public void subir (){
-
+   public void subir(int metros) {
+        if (altitude + metros <= altitudeMaxima) 
+        {
+            altitude += metros;
+            System.out.println(nome + " subiu para " + altitude + " metros.");
+        } else 
+        {
+            System.out.println(nome + " não pode subir além da altitude máxima de " + altitudeMaxima + " metros.");
+        }
     }
 
-    public void descer (){
-
-        
+    public void descer(int metros) 
+    {
+        if (altitude - metros >= 0) 
+        {
+            altitude -= metros;
+            System.out.println(nome + " desceu para " + altitude + " metros.");
+        } else 
+        {
+            System.out.println(nome + " não pode descer mais, pois já está no solo.");
+        }
     }
-
-
 }
