8190f24f7ccaf10ab10fd5e981731bfa8caa1d7c
Júlia Cotrim Chinaglia <136257249+JuliaCotrim18@users.noreply.github.com>
2025-03-26 20:58:58 -0300

    Update Robo_Terrestre.java

--- Robo_Terrestre.java
+++ Robo_Terrestre.java
@@ -11,8 +11,23 @@ public class RoboTerrestre extends Robo{
     {
         super.mover(deltaX, deltaY);
     }
+    
+    @Override // Vamos usar para garantir que o método está de fato sobrescrevendo um método da superclasse
+    public void mover(int deltaX, int deltaY) 
+    {
+        // Calcula a velocidade do robo como a soma dos deslocamentos absolutos
+        int velocidade_movimento = Math.abs(deltaX) + Math.abs(deltaY);
 
-
-
+        // Verifica se a velocidade de movimento é permitida / não ultrapassa a velocidade máxima
+        if (velocidade_movimento <= velocidade_maxima)  // Ocorre movimento
+        {
+            super.mover(deltaX, deltaY);
+            
+        } else // Não há movimento, já que ultrapassa velocidade_maxima
+        {
+            System.out.println("Movimento excede a velocidade máxima permitida!");
+        }
+    }
+}
 
 }
