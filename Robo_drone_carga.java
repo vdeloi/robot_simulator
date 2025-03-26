2ea2c0a82e6693121b0ec2708906d4f51808e019
Júlia Cotrim Chinaglia <136257249+JuliaCotrim18@users.noreply.github.com>
2025-03-26 10:37:26 -0300

    Update Robo_drone_carga.java
    falta comentar

--- Robo_drone_carga.java
+++ Robo_drone_carga.java
@@ -1,1 +1,45 @@
 // Robô Aeréo - Drone de Carga
+
+
+public class Drone_Carga extends Robo_Aereo{
+    
+    private int capacidade_carga;
+    private int carga_atual;
+
+    public Drone_Carga(String nome, String direcao, int posicaoX, int posicaoY, int altitude_maxima, int capacidade_carga)
+    {
+        super(nome, direcao, posicaoX, posicaoY, altitude_maxima);
+        this.capacidade_carga = capacidade_carga;
+        this.carga_atual = 0;
+    }
+
+    public void carregar(int carga)
+    {
+        if (carga_atual + carga <= capacidade_carga)
+        {
+            carga_atual += carga;
+        }
+        else
+        {
+            System.out.println("A carga excede a capacidade do drone!");
+        }
+    }
+
+    public void descarregar(int carga)
+    {
+        if (carga_atual - carga >= 0)
+        {
+            carga_atual -= carga;
+        }
+        else
+        {
+            System.out.println("A carga a ser descarregada é maior que a carga atual!");
+        }
+    }
+
+    public void exibirCarga()
+    {
+        System.out.println("Carga atual: " + this.carga_atual + " de " + this.capacidade_carga);
+    }
+
+}
