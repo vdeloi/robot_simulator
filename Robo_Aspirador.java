3d42359dd8c78e61bbad9af631816e135f967b7d
Júlia Cotrim Chinaglia <136257249+JuliaCotrim18@users.noreply.github.com>
2025-03-26 21:03:09 -0300

    Update Robo_Aspirador.java
    falta comentarr

--- Robo_Aspirador.java
+++ Robo_Aspirador.java
@@ -1,1 +1,34 @@
 // Robô Aspirador
+
+
+public class RoboAspirador extends RoboTerrestre
+{
+    
+    private int capacidade_sujeira;
+    private int sujeira_atual;
+
+    public RoboAspirador(String nome, String direcao, int posicaoX, int posicaoY, int velocidade_maxima, int capacidade_sujeira)
+    {
+        super(nome, direcao, posicaoX, posicaoY, velocidade_maxima);
+        this.capacidade_sujeira = capacidade_sujeira;
+        this.sujeira_atual = 0;
+    }
+
+    public void aspirar(int sujeira)
+    {
+        if (sujeira_atual + sujeira <= capacidade_sujeira)
+        {
+            sujeira_atual += sujeira;
+        }
+        else
+        {
+            System.out.println("A sujeira excede a capacidade do robô aspirador! É necessário limpar o reservatório para continuar.");
+        }
+    }
+
+    public void exibirSujeira()
+    {
+        System.out.println("Sujeira atual: " + this.sujeira_atual + " de " + this.capacidade_sujeira);
+    }
+
+}
