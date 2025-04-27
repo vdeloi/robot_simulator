569ba6899b0c4a80153c2bd57d1d066fa4f3a169
Júlia Cotrim Chinaglia <136257249+JuliaCotrim18@users.noreply.github.com>
2025-04-27 20:12:31 -0300

    Update SensorTemperatura.java

--- SensorTemperatura.java
+++ SensorTemperatura.java
@@ -1,3 +1,21 @@
 // Subclasse Sensor de Temperatura 
 
-// Vamos tentar o mais criativo primeiro
+public class SensorTemperatura extends Sensor {
+
+    public SensorTemperatura(double raio) {
+        super(raio); // Chama o construtor do sensor
+    }
+
+
+     @Override
+    public String monitorar(Ambiente amb, Robo robo) { // Monitora a temperatura presente em um certo raio do robo
+      
+        double temperaturaSimulada = 10.0 + Math.random() * 30.0; // Gera entre 10.0 e < 40.0
+        return String.format("Sensor Temperatura: %.1f °C", temperaturaSimulada);
+    }
+
+    @Override
+    public String toString() {
+        return "SensorTemperatura [raio=" + raio + "]";
+    }
+}
