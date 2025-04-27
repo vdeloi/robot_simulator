72cc412a1d028bd609870faff1cdfa907de781b0
Júlia Cotrim Chinaglia <136257249+JuliaCotrim18@users.noreply.github.com>
2025-04-27 17:27:23 -0300

    Update Sensor.java

--- Sensor.java
+++ Sensor.java
@@ -1,4 +1,33 @@
 // Classe Sensor
 
 
+public abstract class Sensor {
+
+    // Atributo protegido: subclasses podem acessá-lo diretamente.
+    // Representa o alcance máximo do sensor.
+    protected double raio;
+
+ 
+    public Sensor(double raio) {
+      this.raio = raio;
+    }
+
+  
+    public double getRaio() {
+        return raio;
+    }
+
+
+    public abstract String monitorar(Ambiente amb, Robo robo);
+
+    /**
+
+     */
+    @Override
+    public String toString() {
+        return "Sensor [raio=" + raio + "]";
+    }
+}
+
+
 
