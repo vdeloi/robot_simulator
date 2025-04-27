72b81d94913b84c170235c9f048067e55b7430a3
Júlia Cotrim Chinaglia <136257249+JuliaCotrim18@users.noreply.github.com>
2025-04-27 20:17:56 -0300

    Update SensorUmidade.java

--- SensorUmidade.java
+++ SensorUmidade.java
@@ -1,3 +1,20 @@
 // Subclasse Sensor de Umidade
 
-// Acredito que a aplicação será bem similar a da temperatura
+public class SensorUmidade extends Sensor {
+
+    public SensorUmidade(double raio) {
+        super(raio); // Chama o construtor do snesor
+    }
+  
+    @Override
+    public String monitorar(Ambiente amb, Robo robo) {
+      
+        int umidadeSimulada = 20 + (int)(Math.random() * 60); // Vamos fazer uma variação média entre 20% e 80%
+        return String.format("Sensor Umidade: %d%%", umidadeSimulada);
+    }
+
+    @Override
+    public String toString() {
+        return "SensorUmidade [raio=" + raio + "]";
+    }
+}
